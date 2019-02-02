c2827c7b68d2fec0657ea3dae36b75aa0cbfbac3
magic8baller <lturis@gmail.com>
2019-02-01 19:51:30 -0500

    ,

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Welcome from './components/Welcome'
 import ErrorContainer from './components/ErrorContainer'
-// import Navbar from './components/Navbar'
+import Navbar from './components/Navbar'
 import { Link, Route } from 'react-router-dom'
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Route path="/" component={Navbar} />
         <Route exact path="/" component={Welcome} />
         <Route exact path="/error" component={ErrorContainer} />
       </div>
